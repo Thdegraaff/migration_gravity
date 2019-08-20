7b7641aa4053ffcb36199efc274fc0ff6ec41586
Thomas de Graaff <t.de.graaff@vu.nl>
2019-08-20 16:51:36 +0200

    process.results.R

--- code/process_results.R
+++ code/process_results.R
@@ -182,38 +182,45 @@ fit_data <- data.frame(
 
 
 
-fit_large <- filter(fit, Estimate >= 20)
-fit_small <- filter(fit, Estimate < 20)
-hist_fit_small <- ggplot(data = fit_small, aes(Estimate)) + 
-  geom_histogram(col = "black", fill = "forest green", alpha = 0.7, bins = 20) +
-  theme_bw()
-hist_fit_large <- ggplot(data = fit_large, aes(Estimate)) + 
-  geom_histogram(col = "black", fill = "forest green", alpha = 0.7, bins = 20) +
-  scale_x_continuous(breaks=seq(20, 120000, 25000)) +
-  theme_bw()
-hist_fit <- plot_grid(hist_fit_small, hist_fit_large, labels = c("Small flows", "Large flows"), label_x = 0.5, label_y = 0.96) 
+# fit_large <- filter(fit, Estimate >= 20)
+# fit_small <- filter(fit, Estimate < 20)
+# hist_fit_small <- ggplot(data = fit_small, aes(Estimate)) + 
+#   geom_histogram(col = "black", fill = "forest green", alpha = 0.7, bins = 20) +
+#   theme_bw()
+# hist_fit_large <- ggplot(data = fit_large, aes(Estimate)) + 
+#   geom_histogram(col = "black", fill = "forest green", alpha = 0.7, bins = 20) +
+#   scale_x_continuous(breaks=seq(20, 120000, 25000)) +
+#   theme_bw()
+# hist_fit <- plot_grid(hist_fit_small, hist_fit_large, labels = c("Small flows", "Large flows"), label_x = 0.5, label_y = 0.96) 
 
-p <- fit_data %>%
-  ggplot( aes(x=value, fill=type)) +
-  geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity') +
-  scale_fill_manual(values=c("#69b3a2", "#404080")) +
-  theme_ipsum() +
-  labs(fill="")
+# p <- fit_data %>%
+#   ggplot( aes(x=value, fill=type)) +
+#   geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity') +
+#   scale_fill_manual(values=c("#69b3a2", "#404080")) +
+#   theme_ipsum() +
+#   labs(fill="")
 
-fit_large <- filter(fit_data, value >= 20 & value <= 5000)
+fit_large <- filter(fit_data, value >= 20 & value <= 4020)
 fit_small <- filter(fit_data, value < 20)
 hist_fit_small <- ggplot(data = fit_small, aes(value, fill = type)) + 
-                           geom_histogram( color="black", alpha=0.7, position = 'identity' , bins = 20) +
-                           scale_fill_manual(values=c("forest green", "red")) +
-                           theme_bw() +
-                           labs(fill="")
+                         geom_histogram( color="black", alpha=0.7, position = 'dodge' , bins = 20) +
+                         scale_fill_manual(values=c("forest green", "deepskyblue")) +
+                         theme_bw() +
+                         labs(fill="") 
 hist_fit_large <- ggplot(data = fit_large, aes(value, fill = type)) + 
-  geom_histogram( color="#e9ecef", alpha=0.6, position = 'dodge') +
-  scale_fill_manual(values=c("forest green", "red")) +
-  theme_bw() +
-  labs(fill="")
-hist_fit <- plot_grid(hist_fit_small, hist_fit_large, labels = c("Small flows", "Large flows"), label_x = 0.5, label_y = 0.96) 
+                          geom_histogram( color="black", alpha=0.7, position = 'dodge', bins = 20) +
+                          scale_x_continuous(breaks=seq(20, 4020, 1000)) +
+                          scale_fill_manual(values=c("forest green", "deepskyblue")) +
+                          theme_bw() +
+                          labs(fill="")
+hist_fit <- plot_grid(hist_fit_small + theme(legend.position = "none"), 
+                      hist_fit_large + theme(legend.position = "none"), 
+                      labels = c("Small flows", "Large flows"), 
+                      label_x = 0.5, label_y = 0.96) 
 
+legend_b <- get_legend(hist_fit_small + theme(legend.position="bottom"))
+
+hist_fit <- plot_grid( hist_fit, legend_b, ncol = 1, rel_heights = c(1,.1) )
 
 pdf(file = "./fig/hist_fit.pdf" ,width=8,height=4) 
 hist_fit
